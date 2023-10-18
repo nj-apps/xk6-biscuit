ad354217c7c7099eb4afd8ddff43a46770e49796
mxbk7591 <nicolas.jaubertie@orange.com>
2023-10-18 16:26:11 +0200

    update tests

--- biscuit_test.go
+++ biscuit_test.go
@@ -2,6 +2,7 @@ package xk6Biscuit
 
 import (
 	"encoding/base64"
+	"strings"
 	"testing"
 
 	"crypto/rand"
@@ -13,140 +14,61 @@ import (
 	"golang.org/x/crypto/ed25519"
 )
 
-/*
-	type mockVU struct {
-		name string
-	}
-
-// instantiate the interface
-
-	func (mockVU) Context() context.Context {
-		return context.TODO()
-	}
-
-	func (mockVU) InitEnv() *common.InitEnvironment {
-		return nil
-	}
-
-	func (mockVU) State() *lib.State {
-		return nil
-	}
-
-	func (mockVU) Runtime() *goja.Runtime {
-		return &goja.Runtime{}
-	}
-
-	func (mockVU) RegisterCallback() (enqueueCallback func(func() error)) {
-		return enqueueCallback
-	}
-
-	func TestFIFO(t *testing.T) {
-		Arguments := []goja.Value{}
-
-		client1 := newClient(Arguments, mockVU{})
-		client1.Push("first value")
-		client1.Push("2nd value")
-		client2 := newClient(Arguments, mockVU{})
-		out1, _ := client1.Pop()
-		out2, _ := client2.Pop()
-		if out1 != "first value" || out2 != "2nd value" {
-			t.Errorf("Single fifo : out1=%s out2=%s", out1, out2)
-		}
-	}
-
-	func TestNamedFIFO(t *testing.T) {
-		vm := goja.New()
-
-		Arguments := []goja.Value{
-			vm.ToValue("liste A"),
-		}
-		client1 := newClient(Arguments, mockVU{})
-		client1.Push("first value A")
-		client1.Push("2nd value A")
-
-		Arguments = []goja.Value{
-			vm.ToValue("liste B"),
-		}
-		client2 := newClient(Arguments, mockVU{})
-		client2.Push("first value B")
-		client2.Push("2nd value B")
-
-		client3 := newClient(Arguments, mockVU{})
-
-		out1, _ := client1.Pop()
-		out2, _ := client2.Pop()
-		out3, _ := client3.Pop()
-
-		if out1 != "first value A" || out2 != "first value B" || out3 != "2nd value B" {
-			t.Errorf("Named fifos : out1=%s out2=%s out3=%s", out1, out2, out3)
-		}
-
-}
-*/
-func TestBiscuit(t *testing.T) {
-
-	rng := rand.Reader
-	publicRoot, privateRoot, _ := ed25519.GenerateKey(rng)
-	fmt.Print("public:", publicRoot)
-
-	token := createToken(privateRoot)
-	fmt.Printf("token=%v", token)
-
+func TestAttenuate(t *testing.T) {
 	b := Biscuit{}
+
+	// generate a new token
+	rng := rand.Reader
+	_, privateRoot, _ := ed25519.GenerateKey(rng)
+	token := createToken(privateRoot)
+
+	// inspect the token
+	s, e := b.Inspect(token)
+	if e != nil {
+		t.Fatalf("Failed to inspect token: %s", e.Error())
+	}
+	t.Logf("Token details: %s\n", s)
+
+	// Attenuate
 	blocks := []string{
 		"check if time($now), $now < 2023-12-30T15:44:00Z",
 		`check if txn::service("Canal")`}
 
-	t.Error(b.Inspect(token))
-
-	sealed, err := b.Seal(token)
-	if err != nil {
-		t.Fatalf("Error sealing token : %v", err)
+	att, e := b.Attenuate(token, blocks)
+	if e != nil {
+		t.Fatalf("Failed to attenuate token: %s", e.Error())
 	}
 
-	att, err := b.Attenuate(sealed, blocks)
-	if err != nil {
-		t.Fatalf("Error Attenuating token : %v", err)
+	s, _ = b.Inspect(att)
+	if !(strings.Contains(s, "Canal") && strings.Contains(s, "2023-12-30T15:44:00Z")) {
+		t.Fatalf("Failed to attenuate token blocks not found in attenuated token : %s \n", s)
 	}
 
-	t.Errorf("attenuated token=%v", att)
+}
 
-	t.Error(b.Inspect(att))
+func TestSealing(t *testing.T) {
+	b := Biscuit{}
 
-	/*
+	// generate a new token
+	rng := rand.Reader
+	_, privateRoot, _ := ed25519.GenerateKey(rng)
+	token := createToken(privateRoot)
 
-		b0 := Biscuit{}
+	// seal the token
+	sealed, e := b.Seal(token)
+	if e != nil {
+		t.Fatalf("Error sealing token : %s", e.Error())
+	}
 
-		b := Biscuit{
-			checks: []string{
-				"check if time($now), $now < 2023-12-30T15:44:00Z",
-				`check if txn::service("Canal")`},
-		}
+	// try to attenuate
+	_, e = b.Attenuate(sealed, []string{})
+	if e == nil {
+		t.Fatal("Error : attenuate succeed but a sealed token may NOT be Attenuated.")
+	}
 
-		bToken, err := base64.URLEncoding.DecodeString(token)
-		if err != nil {
-			t.Fatalf("Error decoding token : %v", err)
-		}
-
-		att0, err := b0.Attenuate(bToken, &publicRoot)
-		if err != nil {
-			t.Fatalf("Error Attenuating token : %v", err)
-		}
-
-		t.Errorf("original:\n%v\nattenuated:\n%v\n", token, base64.URLEncoding.EncodeToString(att0))
-
-		att, err := b.Attenuate(bToken, &publicRoot)
-		if err != nil {
-			t.Fatalf("Error Attenuating token : %v", err)
-		}
-
-		t.Errorf("original:\n%v\nattenuated:\n%v\n", token, base64.URLEncoding.EncodeToString(att))
-	*/
 }
 
 func createToken(privateRoot ed25519.PrivateKey) string {
-
-	//fmt.Printf("publicRoot=%v", publicRoot)
 
 	authority, err := parser.FromStringBlockWithParams(`
 		right("/a/file1.txt", {read});
@@ -172,10 +94,5 @@ func createToken(privateRoot ed25519.PrivateKey) string {
 		panic(fmt.Errorf("failed to serialize biscuit: %v", err))
 	}
 
-	// token is now a []byte, ready to be shared
-	// The biscuit spec mandates the use of URL-safe base64 encoding for textual representation:
-
-	fmt.Println(base64.URLEncoding.EncodeToString(token))
 	return base64.URLEncoding.EncodeToString(token)
-
 }
